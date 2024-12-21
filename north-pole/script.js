e2a28909dc9ed1beb9c128aa476ee44713e1f6bb
BuildTools <unconfigured@null.spigotmc.org>
2024-12-21 04:55:49 -0500

    c

--- north-pole/script.js
+++ north-pole/script.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function getRandomInt(min, max) {
+    /* inclusive */
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 function adjustDimensions() {
     const sceneContainerDiv = document.getElementById('scene-container');
     const viewportWidth = window.innerWidth;
@@ -15,10 +20,10 @@ function adjustDimensions() {
         newWidth = viewportWidth;
     }
     
-    console.log('viewport width: ' + String(viewportWidth));
+    /*console.log('viewport width: ' + String(viewportWidth));
     console.log('viewport height: ' + String(viewportHeight));
     console.log('new width: ' + String(newWidth));
-    console.log('new height: ' + String(newHeight));
+    console.log('new height: ' + String(newHeight));*/
     
     sceneContainerDiv.style.width = `${newWidth}px`;
     sceneContainerDiv.style.height = `${newHeight}px`;
@@ -42,16 +47,17 @@ toyFactoryImg.classList.add('scene-img');
 
 sceneContainerDiv.appendChild(toyFactoryImg);
 
-
+const conveyorBeltUpdateInterval = 50;
+const conveyorBeltSpeed = 2;
 
 const beltSegmentY = 620;
 const beltSegmentWidth = 164;
 const beltSegmentStartX = -80;
 const beltSegmentXInterval = 160;
-const beltSegmentSpeed = 2;
+
 
 class BeltSegment {
-    constructor(x, y) {
+    constructor(x) {
         this.x = x;
         
         this.img = document.createElement('img');
@@ -68,7 +74,7 @@ class BeltSegment {
     }
     
     move() {
-        this.x += beltSegmentSpeed;
+        this.x += conveyorBeltSpeed;
         this.img.style.left = (this.x / sceneWidth) * 100 + '%';
     }
     
@@ -81,9 +87,9 @@ while (beltSegmentX < sceneWidth) {
     beltSegmentX += beltSegmentXInterval;
 }
 
-function updateConveyerBelt() {
-    console.log(beltSegments.length)
-    if (beltSegments[0].x + beltSegmentSpeed >= 0) {
+/*function updateConveyerBelt() {
+    //console.log(beltSegments.length)
+    if (beltSegments[0].x + conveyorBeltSpeed >= 0) {
         beltSegments.unshift(new BeltSegment(beltSegments[0].x - beltSegmentXInterval));
     }
     
@@ -92,13 +98,113 @@ function updateConveyerBelt() {
     });
     
     const lastBeltSegment = beltSegments[beltSegments.length - 1]
-    console.log('last x ' + lastBeltSegment.x);
-    console.log('scene ' + sceneWidth);
+    //console.log('last x ' + lastBeltSegment.x);
+    //console.log('scene ' + sceneWidth);
     if (lastBeltSegment.x > sceneWidth) {
-        console.log('removing');
+        //console.log('removing');
         lastBeltSegment.img.remove();
         beltSegments.pop();
     }
 }
 
-setInterval(updateConveyerBelt, 50);
+setInterval(updateConveyerBelt, conveyorBeltUpdateInterval);*/
+
+
+const giftBoxBottomY = 664;
+
+const giftBoxData = {
+    'gift-box-1': {
+        'width': 54,
+        'imageWidth': 264,
+        'imageHeight': 292,
+        'yVariance': 12
+    }
+}
+for (const giftBoxKey of Object.keys(giftBoxData)) {
+    const giftBox = giftBoxData[giftBoxKey];
+    giftBox['height'] = (giftBox['imageHeight'] / giftBox['imageWidth']) * giftBox['width'];
+}
+
+class GiftBox {
+    constructor(x, type) {
+        this.x = x;
+        this.type = 'gift-box-1';
+        
+        this.img = document.createElement('img');
+        this.img.src = imagesPath + this.type + '-unopened.png';
+        this.img.classList.add('gift-box');
+        
+        this.img.style.width = (giftBoxData[this.type]['width'] / sceneWidth) * 100 + '%';
+        
+        const yVariance = getRandomInt(0, giftBoxData[this.type]['yVariance']);
+        const y = giftBoxBottomY - giftBoxData[this.type]['height'] - yVariance;
+        
+        this.img.style.top = (y / sceneHeight) * 100 + '%';
+        this.img.style.left = (this.x / sceneWidth) * 100 + '%';
+        this.img.style.zIndex = '3';
+        
+        this.img.addEventListener('click', this.openGift.bind(this), { once: true });
+        
+        sceneContainerDiv.appendChild(this.img);
+        
+    }
+    
+    move() {
+        this.x += conveyorBeltSpeed;
+        this.img.style.left = (this.x / sceneWidth) * 100 + '%';
+    }
+    
+    openGift() {
+        this.img.src = imagesPath + this.type + '-opened.png';
+    }
+}
+
+
+const giftBoxXInterval = 200;
+const giftBoxStartX = -80;
+
+let giftBoxes = [];
+let giftBoxX = giftBoxStartX;
+while (giftBoxX < sceneWidth) {
+    giftBoxes.push(new GiftBox(giftBoxX));
+    giftBoxX += giftBoxXInterval;
+}
+
+function updateConveyerBelt() {
+    //console.log(beltSegments.length)
+    if (beltSegments[0].x + conveyorBeltSpeed >= 0) {
+        beltSegments.unshift(new BeltSegment(beltSegments[0].x - beltSegmentXInterval));
+    }
+    
+    beltSegments.forEach(beltSegment => {
+        beltSegment.move();
+    });
+    
+    const lastBeltSegment = beltSegments[beltSegments.length - 1]
+    //console.log('last x ' + lastBeltSegment.x);
+    //console.log('scene ' + sceneWidth);
+    if (lastBeltSegment.x > sceneWidth) {
+        //console.log('removing');
+        lastBeltSegment.img.remove();
+        beltSegments.pop();
+    }
+    
+    if (giftBoxes[0].x + conveyorBeltSpeed >= 0) {
+        giftBoxes.unshift(new GiftBox(giftBoxes[0].x - giftBoxXInterval));
+    }
+    
+    giftBoxes.forEach(giftBox => {
+        giftBox.move();
+    });
+    
+    const lastGiftBox = giftBoxes[giftBoxes.length - 1]
+    //console.log('last x ' + lastBeltSegment.x);
+    //console.log('scene ' + sceneWidth);
+    if (lastGiftBox.x > sceneWidth) {
+        //console.log('removing');
+        lastGiftBox.img.remove();
+        giftBoxes.pop();
+    }
+}
+
+setInterval(updateConveyerBelt, conveyorBeltUpdateInterval);
